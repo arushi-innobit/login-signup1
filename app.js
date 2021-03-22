e43755e5e7cded5f6e1a71c8f36144f2ed82e731
arushi-innobit <80396720+arushi-innobit@users.noreply.github.com>
2021-03-22 18:09:46 +0530

    2nd commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyparser =require('body-parser');
 const cookieParser =require('cookie-parser');
 const db = require('./config/config').get(process.env.NODE_ENV);
 const User = require('./models/user');
+const bcrypt = require('bcrypt');
 const {auth} = require('./middlewares/auth');
 const { Logger } = require('mongodb');
 const app=express();
@@ -33,7 +34,7 @@ mongoose.connection
 
 //adding the new user , Sign up route
 
-app.post('/api/register',function(req,res)
+app.post('/api/register',async function(req,res)
 {
     const newuser=new User(req.body);
     console.log(newuser);
@@ -63,42 +64,50 @@ app.post('/api/register',function(req,res)
         });
     });
 });
+
+
  // Login route of the user 
- app.post('/api/login', function(req,res){
-    let token=req.cookies.auth;
-    User.findBy(token,(err,user)=>{
-        if(err) 
-        return  res.send(err);
-        if(user) 
-        return res.status(400).json({
-            error :true,
-            message:"You are already logged in"
-        });
-        else{
-            User.findOne({'email':req.body.email},function(err,user){
-                if(!user) return res.json({isAuth : false, message : ' Auth failed ,email not found'});
-        
-                user.comparepassword(req.body.password,(err,isMatch)=>{
-                    if(!isMatch) return res.json({ isAuth : false,message : "password doesn't match"});
-        
-                user.generateToken((err,user)=>{
-                    if(err) return res.status(400).send(err);
-                    res.cookie('auth',user.token).json({
-                        isAuth : true,
-                        id : user._id , 
-                        email : user.email
-                    });
-                });    
-            });
-          });
+ app.post('/api/login', async function(req,res){
+      //let token=req.cookies.auth;
+    const user= await User.findOne({email : req.body.email});
+        if(!user) 
+        {
+        return  res.status(400).json({message: "User does not exist"});
         }
-    });
-});
+        const auth= await bcrypt.compare(password,user.password);
+        if(!auth)
+        { 
+         return res.status(400).json({
+            message:"Password does not match"
+        });
+    }
+        /*else{
+            User.findOne({'email':req.body.email},function(error,User){
+                if(!User) 
+                return res.json({isAuth : false, message : ' Email not found'});
+
+                User.comparepassword(req.body.password,(error,isMatch)=>{
+                    if(!isMatch) return res.json({ isAuth : false,message : "password doesn't match"});
+        */
+                User.generateToken((error,User)=>{
+                    if(!User) return res.status(400);
+                    res.cookie('auth',User.token).json({
+                        isAuth : true,
+                        id : User._id , 
+                        email : User.email
+                    });
+                });
+            });
+          //});
+        
+        //}
+    //} )
+//  });
 
 //Logout of the user and deleting the token
 app.get('/api/logout',auth,function(req,res){
     req.user.deleteToken(req.token,(err,user)=>{
-        if(err) return res.status(400).send(err);
+        if(error) return res.status(400).send(error);
         res.sendStatus(200);
     });
 
@@ -110,6 +119,6 @@ app.get('/',function(req,res){
 
 // listening port
 const PORT=process.env.PORT||3000;
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log(`app is live at ${PORT}`);
 });
--- auth.js
+++ auth.js
@@ -3,17 +3,16 @@ const user=require('./../models/user');
 
 let auth =(req,res,next)=>{
     let token =req.cookies.auth;
-    User.findByToken(token,(err,user)=>{
-        if(err) throw err;
-        if(!user) return res.json({
-            error :true
-        });
-
+    User.findByToken(err,user)
+    {
+        if(err) res.send(err);
+        if(!user) 
+        return res.json({error :true});
         req.token= token;
         req.user=user;
         next();
 
-    })
+    }
 }
 module.exports={auth};
 
